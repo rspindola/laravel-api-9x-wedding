03d90a370dbc16c08d135a596e74e43cd2b0e348
Renato Castro <rspindola@live.com>
2022-11-08 09:33:43 -0300

    feat: add MessageRepositoryInterface in providers

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Contracts\ConfirmationRepositoryInterface;
 use App\Contracts\GodparentsRepositoryInterface;
+use App\Contracts\MessageRepositoryInterface;
 use App\Repositories\ConfirmationRepository;
 use App\Repositories\GodparentsRepository;
+use App\Repositories\MessageRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(ConfirmationRepositoryInterface::class, ConfirmationRepository::class);
         $this->app->bind(GodparentsRepositoryInterface::class, GodparentsRepository::class);
+        $this->app->bind(MessageRepositoryInterface::class, MessageRepository::class);
     }
 
     /**
